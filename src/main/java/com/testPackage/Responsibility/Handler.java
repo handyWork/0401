0fb77f6fd0974e9df1255aa3719bd262478aa263
yangzhi <handywork@163.com>
2019-04-09 21:00:58 +0800

    '责任链模式'

---
+++ src/main/java/com/testPackage/Responsibility/Handler.java
@@ -0,0 +1,8 @@
+package com.testPackage.Responsibility;
+
+/**
+ * 构建一个责任链模式
+ */
+public interface Handler {
+    void operator();
+}
---
+++ src/main/java/com/testPackage/Responsibility/MyHandler.java
@@ -0,0 +1,27 @@
+package com.testPackage.Responsibility;
+
+public class MyHandler extends  AbstractHandler implements  Handler {
+    private String name;
+
+    public MyHandler(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void operator() {
+        System.out.println(name+"deal!");
+        if(getHandler() != null){
+            getHandler().operator();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyHandler h1 = new MyHandler("h1");
+        MyHandler h2 = new MyHandler("h2");
+        MyHandler h3 = new MyHandler("h3");
+        h1.setHandler(h2);
+        h2.setHandler(h3);
+
+        h1.operator();
+    }
+}
