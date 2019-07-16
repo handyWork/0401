88b57b8d9da46f9d384c19bd94f57b7b0a430def
yangzhi <handywork@163.com>
2019-07-16 16:12:56 +0800

    'test'

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 // TODO
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
 
 //1，@EnableDiscoveryClient注解是基于spring-cloud-commons依赖，并且在classpath中实现；
 //		2，@EnableEurekaClient注解是基于spring-cloud-netflix依赖，只能为eureka作用；
@@ -15,7 +16,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(DemoApplication.class, args);
 
 	}
