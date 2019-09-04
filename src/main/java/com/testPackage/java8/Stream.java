66053f6b2a78b4718a70863a03e65871a4e2daae
yangzhi <handywork@163.com>
2019-09-04 16:27:23 +0800

    java8 中流过滤代替循环遍历

--- src/main/java/com/testPackage/java8/Stream.java
+++ src/main/java/com/testPackage/java8/Stream.java
@@ -1,6 +1,9 @@
 package com.testPackage.java8;
 
+import org.assertj.core.util.Lists;
+
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Stream {
 
@@ -44,6 +47,28 @@ private void add(String string){
         Stream stream = new Stream();
         int sum = stream.tasks.stream().filter(task -> task.getStatus() == Status.OPEN).mapToInt(Task::getPoints).sum();
         System.out.println("值的总和为"+sum);
+
+
+        List<String> subordinates = Lists.newArrayList();
+        subordinates.add("1");
+        subordinates.add("2");
+        subordinates.add("4");
+        subordinates.add("5");
+        subordinates.add("6");
+
+        List<String> concernIds = Lists.newArrayList();
+        concernIds.add("1");
+        concernIds.add("2");
+        concernIds.add("3");
+        concernIds.add("4");
+        // item  是指循环concernIds中的当前的那个元素
+        List<String> collect = concernIds.stream().filter(item -> subordinates.contains(item)).distinct()
+                .collect(Collectors.toList());
+        System.out.println("subordinates所包含的元素"+collect);
+        // 剔除掉元素为2的集合
+        List<String> collect2 = concernIds.stream().filter(item -> subordinates.contains(item)).filter(item -> !item.equals("2")).distinct()
+                .collect(Collectors.toList());
+        System.out.println("剔除后的集合为"+collect2);
     }
 
 
